aa659e8150f0e756da317a9de8cc480468746614
Galen Rice <griceturrble@protonmail.com>
2022-08-13 19:28:18 -0400

    ci: build site completely within GH actions
    Later we should be able to augment this with the Tailwind build chain
    and any other tooling we want.

---
+++ tools/test.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#
+# Build and test the site content
+#
+# Requirement: html-proofer, jekyll
+#
+# Usage: See help information
+
+set -eu
+
+SITE_DIR="_site"
+
+_config="_config.yml"
+
+help() {
+  echo "Build, test and then deploy the site content to 'origin/<pages_branch>'"
+  echo
+  echo "Usage:"
+  echo
+  echo "   bash ./tools/deploy.sh [options]"
+  echo
+  echo "Options:"
+  echo '     -c, --config   "<config_a[,config_b[...]]>"    Specify config file(s)'
+  echo "     -h, --help               Print this information."
+}
+
+build() {
+  # clean up
+  if [[ -d $SITE_DIR ]]; then
+    rm -rf "$SITE_DIR"
+  fi
+
+  # build
+  JEKYLL_ENV=production bundle exec jekyll build --destination "$SITE_DIR" --config "$_config"
+}
+
+test() {
+  bundle exec htmlproofer \
+    --disable-external \
+    --allow_hash_href \
+    "$SITE_DIR"
+}
+
+main() {
+  build
+  test
+}
+
+while (($#)); do
+  opt="$1"
+  case $opt in
+  -c | --config)
+    _config="$2"
+    shift
+    shift
+    ;;
+  -h | --help)
+    help
+    exit 0
+    ;;
+  *)
+    # unknown option
+    help
+    exit 1
+    ;;
+  esac
+done
+
+main
